2d65c67e961d07f022041c18f2f0e9626d1d9e26
Tobias Plum <tobias@fam-plum.de>
2024-01-10 00:34:46 +0100

    script prototype working

--- abidownload.py
+++ abidownload.py
@@ -2,30 +2,60 @@
 
 from pdfminer.high_level import extract_text
 from webdav3.client import Client
+import os
+import re
 
-# PDF parsen
+input_directory = "testdaten/ausgewählte"
+output_directory = "testdaten/klausuren"
 
-pdfcontent = extract_text(filename).splitlines()
-fach_name = pdfcontent[0][:-1]
-jahr = pdfcontent[2][:4]
-share_id = pdfcontent[2].split("/")[-1][:-1]
-share_password = pdfcontent[4][:-1]
+# find all pdfs
 
+credentials_pdfs = [
+    f
+    for f in os.listdir(input_directory)
+    if re.match(r"\d{4}-\d{2}-\d{2}_\d+-(.+)+\.pdf", f)
+]
 
-# Download aus ownCloud
+documentation = {}
 
-link_to_cloud = "https://membox.nrw.de/remote.php/dav/public-files/"
+for pdf in credentials_pdfs:
+    # parse pdf
 
-options = {
-    "webdav_hostname": link_to_cloud + share_id,
-    "webdav_login": "public",
-    "webdav_password": share_password,
-}
+    pdfcontent = extract_text(os.path.join(input_directory, pdf)).splitlines()
+    subject = pdfcontent[0][:-1]
+    year = pdfcontent[2][:4]
+    share_id = pdfcontent[2].split("/")[-1][:-1]
+    share_password = pdfcontent[4][:-1]
 
-client = Client(options)
-filenames = client.list()
+    # download from ownCloud
 
-for file in filenames[1:]:
-    # Wirft Fehler, da Ordner nicht existieren:
-    # client.download_sync(remote_path=file, local_path=f"{file[2:4]}/{file[0:2]}/{file}")
-    client.download_sync(remote_path=file, local_path=f"{file}")
+    download_location = os.path.join(output_directory, subject, year)
+    try:
+        # create download directory
+        os.makedirs(download_location)
+    except FileExistsError:
+        pass
+
+    options = {
+        "webdav_hostname": "https://membox.nrw.de/remote.php/dav/public-files/" + share_id,
+        "webdav_login": "public",
+        "webdav_password": share_password,
+    }
+
+    client = Client(options)
+    filenames = client.list()
+    for file in filenames[1:]:
+        client.download_sync(remote_path=file, local_path=os.path.join(download_location, file))
+        try:
+            documentation[subject][year].append(file)
+        except KeyError: # year does not exist for subject in documentation
+            try:
+                documentation[subject][year] = []
+                documentation[subject][year].append(file)
+            except KeyError: # subject does not exist in documentation
+                documentation[subject] = {}
+                documentation[subject][year] = []
+                documentation[subject][year].append(file)
+        
+
+print(documentation)

511e1a1860aba953b830f404832dd9a0a5946d2d
Tobias Plum <tobias@fam-plum.de>
2024-01-10 23:03:06 +0100

    reformatted

--- abidownload.py
+++ abidownload.py
@@ -6,39 +6,38 @@ from webdav3.client import Client
 from webdav3.exceptions import WebDavException
 import os
 import re
-import json
+
 
 def find_all_credential_pdfs(path):
     credential_pdfs = [
-        f
-        for f in os.listdir(path)
-        if re.match(r"\d{4}-\d{2}-\d{2}_\d+-(.+)+\.pdf", f)
+        f for f in os.listdir(path) if re.match(r"\d{4}-\d{2}-\d{2}_\d+-(.+)+\.pdf", f)
     ]
     return credential_pdfs
 
+
 def parse_credential_pdf(path_to_file):
     pdfcontent = extract_text(path_to_file).splitlines()
-    subject = re.search(r'([^\/]+)', pdfcontent[0]).group(1)[:-1]
+    subject = re.search(r"([^\/]+)", pdfcontent[0]).group(1)[:-1]
     year = pdfcontent[2][:4]
     share_id = pdfcontent[2].split("/")[-1][:-1]
     share_password = pdfcontent[4][:-1]
     return subject, year, share_id, share_password
 
-def download_all_files_from_share(share_id, share_password, subject, year, download_directory):
+
+def download_all_files_from_share(
+    share_id, share_password, subject, year, download_directory
+):
     download_location = os.path.join(download_directory, subject, year)
-    try:
-        # create download directory
-        os.makedirs(download_location)
-    except FileExistsError:
-        pass
 
     options = {
-        "webdav_hostname": "https://membox.nrw.de/remote.php/dav/public-files/" + share_id,
+        "webdav_hostname": "https://membox.nrw.de/remote.php/dav/public-files/"
+        + share_id,
         "webdav_login": "public",
         "webdav_password": share_password,
     }
-
     client = Client(options)
+
+    # list all available files in share
     sucessful, i = False, 0
     while not sucessful:
         try:
@@ -47,20 +46,32 @@ def download_all_files_from_share(share_id, share_password, subject, year, downl
         except WebDavException as exception:
             i += 1
             if i == 5:
-                print(f"""Failed fetching shared files!
+                print(
+                    f"""Failed fetching shared files!
                 Share-ID: {share_id}
                 Share Password: {share_password}
                 Subject: {subject}
-                Year: {year}""")
+                Year: {year}"""
+                )
                 return
             print(f"Retry... ({subject})")
             pass
+
+    # create download directory
+    try:
+        os.makedirs(download_location)
+    except FileExistsError:
+        pass
+
+    # download all files in share
     for file in filenames[1:]:
         print(f"Downloading {subject} {year}: {file}")
         sucessful, i = False, 0
         while not sucessful:
             try:
-                client.download_sync(remote_path=file, local_path=os.path.join(download_location, file))
+                client.download_sync(
+                    remote_path=file, local_path=os.path.join(download_location, file)
+                )
                 sucessful = True
             except WebDavException as exception:
                 i += 1
@@ -69,6 +80,7 @@ def download_all_files_from_share(share_id, share_password, subject, year, downl
                     break
                 print(f"Retry... ({file})")
                 pass
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -83,11 +95,27 @@ def main():
 
     credential_pdfs = find_all_credential_pdfs(input_directory)
     for pdf in credential_pdfs:
-        subject, year, share_id, share_password = parse_credential_pdf(os.path.join(input_directory, pdf))
-        credentials.append({"subject": subject, "year": year, "share_id": share_id, "share_password": share_password})
-    
+        subject, year, share_id, share_password = parse_credential_pdf(
+            os.path.join(input_directory, pdf)
+        )
+        credentials.append(
+            {
+                "subject": subject,
+                "year": year,
+                "share_id": share_id,
+                "share_password": share_password,
+            }
+        )
+
     for credential in credentials:
-        download_all_files_from_share(credential["share_id"], credential["share_password"], credential["subject"], credential["year"], output_directory)
+        download_all_files_from_share(
+            credential["share_id"],
+            credential["share_password"],
+            credential["subject"],
+            credential["year"],
+            output_directory,
+        )
+
 
 if __name__ == "__main__":
     main()
